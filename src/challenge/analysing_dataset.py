f29920df067e5aa017607c05184d711db8cb44ed
Venkataramana Radhakrishnan <venkat@zendoc.com>
2019-09-08 22:45:16 +0200

    analysing data

---
+++ src/challenge/analysing_dataset.py
@@ -0,0 +1,39 @@
+import argparse
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from mpl_toolkits import mplot3d
+
+def analyse(args):
+  filepath_1 = os.path.join(args.input_path,'gwlq.csv')
+  filepath_2 = os.path.join(args.input_path,'catalog.csv')
+  df_1 = pd.read_csv(filepath_1)
+  df_2 = pd.read_csv(filepath_2)
+  fig = plt.figure()
+  ax = plt.axes(projection='3d')
+  xdata = df_1['x'].values
+  ydata = df_1['y'].values
+  zdata = df_1['z'].values
+  ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='Greens')
+  plt.show()
+  print()
+  df_temp = df_1[df_1['z'] == 0]
+  fig = plt.figure()
+  ax = plt.axes(projection='3d')
+  xdata = df_temp['x'].values
+  ydata = df_temp['y'].values
+  zdata = df_temp['z'].values
+  ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='Greens')
+  plt.show()
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--input_path",
+                        type=str,
+                        required=False,
+                        default=r'C:\Users\venks\Downloads\Others\datasets',
+                        help="Data source path for the csv files")
+  args = parser.parse_args()
+  analyse(args)
